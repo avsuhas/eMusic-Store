47c4f8f1463b8cffede10ee24780d000a3110864
Suhas V Arudi <avsuhas@gmail.com>
2017-07-06 20:58:38 -0400

    Add files via upload

---
+++ dao/CustomerOrderDao.java
@@ -0,0 +1,12 @@
+package com.emusicstore.dao;
+
+import com.emusicstore.model.CustomerOrder;
+
+/**
+ * Created by suhasarudi on 07/27/2017.
+ */
+public interface CustomerOrderDao {
+
+    void addCustomerOrder(CustomerOrder customerOrder);
+
+}
